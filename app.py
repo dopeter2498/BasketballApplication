40492eebbcf44c8211e2dfa5803b3a01ef98f20e
Peter Do <peterdo@Peters-MBP.T-mobile.com>
2021-09-27 19:55:12 -0700

    Added templates folder. Added home and draft pages.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_roster_stats, get_team_misc
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/drafts")
+def drafts():
+    return print(get_roster('LAL',2019))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
