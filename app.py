c1d9d5dbc77d2e9c33aaee27e3a4cc724a4c8cad
Peter Do <peterdo@Peters-MacBook-Pro.local>
2021-09-29 17:14:18 -0700

    added navigation bar along with corresponding html files

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 from flask import Flask, render_template
 from basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_roster_stats, get_team_misc
+from flask_bootstrap import Bootstrap
+
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+@app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+@app.route("/players")
+def players():
+    return render_template('players.html')
+
+@app.route("/teams")
+def teams():
+    return render_template('teams.html')
+
+@app.route("/seasons")
+def seasons():
+    return render_template('seasons.html')
+
+@app.route("/leaders")
+def leaders():
+    return render_template('leaders.html')
+
+@app.route("/scores")
+def scores():
+    return render_template('scores.html')
+
 @app.route("/drafts")
 def drafts():
-    return print(get_roster('LAL',2019))
+    return render_template('drafts.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
