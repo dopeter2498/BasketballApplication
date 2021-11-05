9d6911803eafdec654d95402ea13506432092f38
Peter Do <dopeter2498@gmail.com>
2021-11-05 16:03:15 -0700

    Created teams page to list teams for the current season. Team names will redirect to their team's page on click.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from basketball_reference_scraper import seasons as s
+from basketball_reference_scraper import teams as t
+from basketball_reference_scraper import constants 
+from basketball_reference_scraper.constants import TEAM_TO_TEAM_ABBR
+from datetime import date
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 
 @app.route("/")
@@ -13,9 +19,21 @@ def home():
 def players():
     return render_template('players.html')
 
-@app.route("/teams")
-def teams():
-    return render_template('teams.html')
+@app.route("/teams", methods=['GET', 'POST'])
+def teams():                                       
+    data = s.get_standings()
+    return render_template('teams.html', data=data)
+
+@app.route("/teams-result/<string:team>", methods=['GET', 'POST'])
+def teams_result(team):
+    currYear = date.today().year
+    if (date.today().month > 7):
+        currYear += 1
+    for key in TEAM_TO_TEAM_ABBR:
+        if key == team.upper():
+            teamAbbr = TEAM_TO_TEAM_ABBR[key]
+    roster = t.get_roster_stats(teamAbbr,currYear)
+    return render_template('/teams-result.html', roster=roster, team=team)
 
 @app.route("/seasons")
 def seasons():
